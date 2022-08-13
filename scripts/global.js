aece09fa64a23c2cec492fe1b4858ab57f558ddd
Crab55e <99100129+Crab55e@users.noreply.github.com>
2022-08-13 19:01:39 +0900

    Add files via upload

---
+++ scripts/global.js
@@ -0,0 +1,6 @@
+document.querySelector('.menu-btn').addEventListener('click', function() {
+    document.querySelector('.menu').classList.toggle('is-active');
+});
+document.querySelector('.menu-btn').addEventListener('click', function() {
+    document.querySelector('.menu-btn').classList.toggle('is-opened');
+});
